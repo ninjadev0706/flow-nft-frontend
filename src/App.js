8d6d21082e29e2abb0bb50e21aba2efae11a3e8e
ninjadev0706 <ninjadeveloper0706@gmail.com>
2022-07-02 19:10:05 -0700

    integrated with wallet

--- src/App.js
+++ src/App.js
@@ -2,14 +2,65 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
 import twitterLogo from "./assets/twitter-logo.svg";
+import * as fcl from "@onflow/fcl";
+import * as types from "@onflow/types";
 
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+fcl.config({
+  "flow.network": "testnet",
+  "app.detail.title": "BottomShot", // Change the title!
+  "accessNode.api": "https://rest-testnet.onflow.org",
+  "app.detail.icon": "https://placekitten.com/g/200/200",
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
+});
+
 function App() {
+
+  const [ user, setUser ] = useState();
+
+  const logIn = () => {
+    fcl.authenticate();
+  };
+
+  const logOut = () => {
+    fcl.unauthenticate();
+  };
+  
+  useEffect(() => {
+    // This listens to changes in the user objects
+    // and updates the connected user
+    fcl.currentUser().subscribe(setUser);
+  }, [])
+
+  const RenderLogin = () => {
+    return (
+      <div>
+        <button className="cta-button button-glow" onClick={() => logIn()}>
+          Log In
+        </button>
+      </div>
+    );
+  };
+
+  const RenderLogout = () => {
+    if (user && user.addr) {
+      return (
+        <div className="logout-container">
+          <button className="cta-button logout-btn" onClick={() => logOut()}>
+            ❎ {"  "}
+            {user.addr.substring(0, 6)}...{user.addr.substring(user.addr.length - 4)}
+          </button>
+        </div>
+      );
+    }
+    return undefined;
+  };
 
   return (
     <div className="App">
+      <RenderLogout />
       <div className="container">
         <div className="header-container">
           <div className="logo-container">
@@ -19,6 +70,9 @@ function App() {
 
           <p className="sub-text">The easiest NFT mint experience ever!</p>
         </div>
+
+        {/* If not logged in, render login button */}
+        {user && user.addr ? "Wallet connected!" : <RenderLogin />}
 
         <div className="footer-container">
             <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
